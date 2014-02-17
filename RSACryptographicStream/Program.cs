83cb201ef2bba67103e2f0c132a451b3af6b25da
Steven Swenson <ctigeek@gmail.com>
2014-02-17 16:30:10 -0500

    Updated example code in Program.cs.

--- RSACryptographicStream/Program.cs
+++ RSACryptographicStream/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
+using System.Linq;
 
 namespace RSACryptographicStream
 {
@@ -14,39 +15,37 @@ namespace RSACryptographicStream
             {
                 //This creates the RSA keys. In a production environment, you'll probably be using key files.
                 // One of the advantages of using RSA is you can have the public key used for encryption on your web servers, and keep the private key safe somewhere that's not on a public-facing server.
-                var rsaProvider = new RSACryptoServiceProvider();
+                var rsaKey = new RSACryptoServiceProvider();
 
-                //create a stream with test data....
-                string testData = "some random string.";
-                MemoryStream source = new MemoryStream();
-                StreamWriter writer = new StreamWriter(source);
-                writer.AutoFlush = true;
-                writer.Write(testData);
-
-                source.Seek(0, SeekOrigin.Begin);
+                string encryptThisString = GetString(2000);
+                Stream source = Streamify(encryptThisString);
 
                 //Encrypt the stream....
-                var encryptedData = new MemoryStream();
-                var encryptor = new RSAEncryptorStreamWriter(encryptedData, rsaProvider);
-                source.CopyTo(encryptor);
-                encryptor.Flush(); // Important! You must flush or dispose any stream writer ...the underlying stream will not have the last chunk of data until that happens!
+                var destination = new MemoryStream();
+                
+                //Magic!
+                Encrypt(source, destination, rsaKey);
 
-                encryptedData.Seek(0, SeekOrigin.Begin);
+                destination.Seek(0, SeekOrigin.Begin);
 
                 //decrypt to a new stream...
                 var decryptedData = new MemoryStream();
-                var decryptor = new RSADecrypterStreamReader(encryptedData, rsaProvider);
-                decryptor.CopyTo(decryptedData);
-                decryptedData.Flush();
-                decryptedData.Seek(0, SeekOrigin.Begin);
                 
-                //read into a string...
-                StreamReader reader = new StreamReader(decryptedData);
-                var decryptedString = reader.ReadToEnd();
+                //more Magic!
+                Decrypt(destination, decryptedData, rsaKey);
+                
 
-                Console.WriteLine("Original string:{0}", testData);
-                Console.WriteLine("Decrypted string:{0}", decryptedString);
+                //Turn back into a string....
+                decryptedData.Seek(0, SeekOrigin.Begin);
+                string decryptedString = Stringify(decryptedData);
 
+                //Console.WriteLine("Original string:{0}", encryptThisString);
+                //Console.WriteLine("Decrypted string:{0}", decryptedString);
+
+                if (encryptThisString == decryptedString)
+                {
+                    Console.WriteLine("Hooray, they match.");
+                }
                 //Yes, I know I'm not disposing properly... it's just test code...
             }
             catch (Exception ex)
@@ -56,5 +55,45 @@ namespace RSACryptographicStream
             Console.WriteLine("Press enter to close.");
             Console.ReadLine();
         }
+
+        private static void Encrypt(Stream source, Stream destination, RSACryptoServiceProvider key)
+        {
+            var encryptor = new RSAEncryptorStreamWriter(destination, key);
+            source.CopyTo(encryptor);
+            encryptor.Flush(); // Important! You must flush or dispose any stream writer ...the underlying stream will not have the last chunk of data until that happens!
+        }
+
+        private static void Decrypt(Stream source, Stream destination, RSACryptoServiceProvider key)
+        {
+            var decryptor = new RSADecrypterStreamReader(source, key);
+            decryptor.CopyTo(destination);
+            destination.Flush();
+        }
+
+        const string LoremIpsum = @"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        private static string GetString(int numOfCharacters)
+        {
+            int count = (numOfCharacters / LoremIpsum.Length) + 1;
+            string testData = String.Join(Environment.NewLine, Enumerable.Repeat(LoremIpsum, count));
+            return testData;
+        }
+        private static Stream Streamify(string str)
+        {
+            //create a stream with test data....
+            MemoryStream source = new MemoryStream();
+            StreamWriter writer = new StreamWriter(source);
+            writer.AutoFlush = true;
+            writer.Write(str);
+
+            source.Seek(0, SeekOrigin.Begin);
+            return source;
+        }
+        private static string Stringify(Stream stream)
+        {
+            StreamReader reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
+
+
     }
 }
